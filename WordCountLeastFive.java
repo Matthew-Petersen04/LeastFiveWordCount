26c011721f901c1fc2534fdd76cf7a8aa7be3189
Matthew-Petersen04 <43018151+Matthew-Petersen04@users.noreply.github.com>
2021-04-21 16:56:51 -0400

    Add files via upload
    Got the least five wordcount program working and uploading files

--- WordCountLeastFive.java
+++ WordCountLeastFive.java
@@ -1,4 +1,3 @@
-import java.net.URI;
 import java.io.*;
 import java.util.*;
 
@@ -11,16 +10,14 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.Counter;
 import org.apache.hadoop.util.GenericOptionsParser;
-import org.apache.hadoop.util.StringUtils;
 
 public class WordCountLeastFive {
 
-    public static class TokenizerMapper
-            extends Mapper<Object, Text, Text, LongWritable>{
+    public static class LFMapper
+            extends Mapper<Object, Text, Text, LongWritable> {
 
-        static enum CountersEnum { INPUT_WORDS }
+        private TreeMap<String, Long> tmap;
 
         private final static LongWritable one = new LongWritable(1);
         private Text word = new Text();
@@ -34,6 +31,8 @@ public class WordCountLeastFive {
         @Override
         public void setup(Context context) throws IOException,
                 InterruptedException {
+
+            /*
             conf = context.getConfiguration();
             caseSensitive = conf.getBoolean("wordcount.case.sensitive", true);
             if (conf.getBoolean("wordcount.skip.patterns", false)) {
@@ -44,8 +43,11 @@ public class WordCountLeastFive {
                     parseSkipFile(patternsFileName);
                 }
             }
+             */
+            tmap = new TreeMap<String, Long>();
         }
 
+        /*
         private void parseSkipFile(String fileName) {
             try {
                 fis = new BufferedReader(new FileReader(fileName));
@@ -58,73 +60,146 @@ public class WordCountLeastFive {
                         + StringUtils.stringifyException(ioe));
             }
         }
+        */
 
         @Override
-        public void map(Object key, Text value, Context context
-        ) throws IOException, InterruptedException {
-            String line = (caseSensitive) ?
-                    value.toString() : value.toString().toLowerCase();
-            for (String pattern : patternsToSkip) {
-                line = line.replaceAll(pattern, "");
+        public void map(Object key, Text value,
+                        Context context) throws IOException,
+                InterruptedException {
+
+            //input data is a "sentence" of words separated by spaces
+            String[] tokens = value.toString().split(" ");
+
+            //goes through the words and adds them to the tmap
+            //if the word is not contained in tmap set its value to 1
+            //if it does exist take the value, add 1 to it, and update it in tmap
+
+            for (String word : tokens) {
+                if (!tmap.containsKey(word)) {
+                    tmap.put(word, (long) 1);
+                } else {
+                    long update = tmap.get(word) + 1;
+                    tmap.put(word, update);
+                }
             }
-            StringTokenizer itr = new StringTokenizer(line);
-            while (itr.hasMoreTokens()) {
-                word.set(itr.nextToken());
-                context.write(word, one);
-                Counter counter = context.getCounter(CountersEnum.class.getName(),
-                        CountersEnum.INPUT_WORDS.toString());
-                counter.increment(1);
+
+            // we remove the first key-value
+            // if it's size increases 10
+            //if (tmap.size() > 10) {
+            //    tmap.remove(tmap.firstKey());
+            //}
+        }
+
+        @Override
+        public void cleanup(Context context) throws IOException,
+                InterruptedException {
+            for (Map.Entry<String, Long> entry : tmap.entrySet()) {
+
+                long count = entry.getValue();
+                String word = entry.getKey();
+
+                context.write(new Text(word), new LongWritable(count));
             }
         }
     }
 
-    public static class IntSumReducer
-            extends Reducer<Text,LongWritable,Text,LongWritable> {
-        private LongWritable result = new LongWritable();
-        static int num = 5;
+    public static class LFReducer
+            extends Reducer<Text, LongWritable, Text, LongWritable> {
+        //private LongWritable result = new LongWritable();
+        private TreeMap<Long, ArrayList<String>> tmap2;
+
+        @Override
+        public void setup(Context context) throws IOException,
+                InterruptedException {
+            tmap2 = new TreeMap<Long, ArrayList<String>>();
+        }
+
+        @Override
         public void reduce(Text key, Iterable<LongWritable> values,
-                           Context context
-        ) throws IOException, InterruptedException {
-            int sum = 0;
+                           Context context) throws IOException, InterruptedException {
+
+            // input data from mapper
+            // key                values
+            // movie_name         [ count ]
+            String word = key.toString();
+            long count = 0;
+
             for (LongWritable val : values) {
-                sum += val.get();
+                count += val.get();
             }
-            if(num > 0) {
-                result.set(sum);
-                context.write(key, result);
-                num --;
+
+            // insert data into treeMap,
+            // we want top 10 viewed movies
+            // so we pass count as key
+            //tmap2.put(count, name);
+
+            if (!tmap2.containsKey(count)) {
+                ArrayList<String> toAdd = new ArrayList<String>();
+                tmap2.put(count, new ArrayList<String>());
+            }
+
+            //add to the arraylist all words that have the same count
+            tmap2.get(count).add(word);
+
+
+            // we remove the first key-value
+            // if it's size increases 5 (for least 5)
+            if (tmap2.size() > 5) {
+                tmap2.remove(tmap2.lastKey());
+            }
+        }
+
+        @Override
+        public void cleanup(Context context) throws IOException,
+                InterruptedException {
+            int n = 5; //least 5 n cannot be in the method
+
+            for (Map.Entry<Long, ArrayList<String>> entry : tmap2.entrySet()) {
+                long count = entry.getKey();
+                ArrayList<String> words = entry.getValue();
+                for (int i = 0; i < words.size(); i++) {
+                    if (n > 0) {
+                        String word = words.get(i);
+                        context.write(new Text(word), new LongWritable(count));
+                        n--;
+                    }
+                }
             }
         }
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception{
+
         Configuration conf = new Configuration();
-        GenericOptionsParser optionParser = new GenericOptionsParser(conf, args);
-        String[] remainingArgs = optionParser.getRemainingArgs();
-        if ((remainingArgs.length != 2) && (remainingArgs.length != 4)) {
-            System.err.println("Usage: wordcount <in> <out> [-skip skipPatternFile]");
+        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+
+        if(otherArgs.length < 2) {
+
+            // if arguments are not provided
+            System.err.println("Error: please provide two paths");
             System.exit(2);
         }
-        Job job = Job.getInstance(conf, "word count");
-        job.setJarByClass(WordCountLeastFive.class);
-        job.setMapperClass(TokenizerMapper.class);
-        job.setCombinerClass(IntSumReducer.class);
-        job.setReducerClass(IntSumReducer.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(LongWritable.class);
 
-        List<String> otherArgs = new ArrayList<String>();
-        for (int i=0; i < remainingArgs.length; ++i) {
-            if ("-skip".equals(remainingArgs[i])) {
-                job.addCacheFile(new Path(remainingArgs[++i]).toUri());
-                job.getConfiguration().setBoolean("wordcount.skip.patterns", true);
-            } else {
-                otherArgs.add(remainingArgs[i]);
-            }
-        }
-        FileInputFormat.addInputPath(job, new Path(otherArgs.get(0)));
-        FileOutputFormat.setOutputPath(job, new Path(otherArgs.get(1)));
+        Job job = Job.getInstance(conf, "Least Five Word Count");
+        job.setJarByClass(WordCountLeastFive.class);
+
+        job.setMapperClass(LFMapper.class);
+        job.setReducerClass(LFReducer.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(LongWritable.class);
+
+        job.setNumReduceTasks(1);
+
+        job.setOutputKeyClass(LongWritable.class);
+        job.setOutputValueClass(Text.class);
+
+        FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
 
         System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+
+
     }
-}
+    }
